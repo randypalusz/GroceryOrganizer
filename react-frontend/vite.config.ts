265f3e0590d655840059808a37206e1ed1ddd7d0
randypalusz <ibanezguy998@gmail.com>
2023-10-15 16:18:42 -0400

    add comment for host property in vite.config.ts

--- react-frontend/vite.config.ts
+++ react-frontend/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    // settings to true allows the host to listen on all IP addresses
+    // https://vitejs.dev/config/server-options.html
     host: true,
     // TODO: unsure what to set here, I think default is 4173
     port: 3000
